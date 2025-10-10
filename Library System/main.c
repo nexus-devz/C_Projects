50e8a6bfaec52e553dfadaa774d3fe093e5397ea
nexus-devz <fahimabrarmahir@gmail.com>
2025-10-10 16:34:12 +0600

    updated main.c

--- Library System/main.c
+++ Library System/main.c
@@ -1,2 +1,16 @@
+//Library system
+
+#include<stdio.h>
+typedef struct {
+    char name[50], pass[50];
+    int id, booksKept, booksReturned;
+}User;
+
 int main(){
+    int opt ;
+    User user[1000];
+    
+    printf("\tWelcome to Ist Library\nPlease Input your user name and password");
+
+
 }
