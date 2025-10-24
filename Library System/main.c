ba537b8a3d11bcd298eb9f1cbd43cd1b100e625d
nexus-devz <fahimabrarmahir@gmail.com>
2025-10-24 23:39:04 +0600

    Refactor user input handling and enhance search functionality in main.c

--- Library System/main.c
+++ Library System/main.c
@@ -13,7 +13,7 @@ typedef struct {
 int isLoggedIn();
 void registerUser(int* userNum, User *U);
 void searchUser();
-
+void profile(int n[], int c, User users[]);
 int main(){
     char opt ;
     int userCount = 0; // for now
@@ -27,7 +27,7 @@ int main(){
             printf("Main menu\n");
             printf("\t1. Users\n\t2. Books\n\t3. Quit\n");
             printf("Enter the option number of your choice: ");
-        scanf(" %c", &opt);
+        scanf("  %c", &opt);
 
         switch(opt){
 
@@ -37,7 +37,7 @@ int main(){
                 printf("Choose from below:\n");
                 printf("\t1. Register Users\n\t2. Search user-profile\n\t3. Back\n");
                 printf("Enter the option number of your choice: ");
-            scanf(" %c", &ch);
+            scanf("  %c", &ch);
             switch(ch){
             case '1':
                 registerUser(&userCount, user);
@@ -72,9 +72,9 @@ int isLoggedIn(){
     char name[50], pass[50];
     while(try--){
         printf("Username: ");
-        scanf("%s", name);
+        scanf(" %s", name);
         printf("Password: ");
-        scanf("%s", pass);
+        scanf(" %s", pass);
         if(strcmp(name,adminName) == 0 && strcmp(pass,adminPass) == 0){
             printf("Successfully logged in\n");
             return 1;
@@ -90,37 +90,97 @@ int isLoggedIn(){
 void registerUser(int* userNum, User *U){
     int n, u= *userNum;
     printf("Enter the number of users you want to register: ");
-    scanf("%d", &n);
+    scanf(" %d", &n);
     //need to comp n and u
     for(int i=0; i<n; i++){
         
         printf("user %d:\n", i+1);
         printf("Input name: ");
-        scanf("%s", &(U+u+i)->name);
+        scanf(" %s", &(U+u+i)->name);
         printf("Input id: ");
-        scanf("%d", &(U+u+i)->id);
+        scanf(" %d", &(U+u+i)->id);
         printf("Input department: ");
-        scanf("%s", &(U+u+i)->dept);
+        scanf(" %s", &(U+u+i)->dept);
         printf("Input semester: ");
-        scanf("%d", &(U+u+i)->sem);
+        scanf(" %d", &(U+u+i)->sem);
     }
     *userNum+=n;
     printf("Registration has been completed\n");
 }
 
 void searchUser(int* userNum, User users[]){
-    printf("Search by:\n1. Name\n2. id");
-    int c, n[*userNum];
-    scanf("%d",&c);
+    printf("Search by:\n1. Name\n2. id\n:");
+    int c, n[*userNum]; //temporary array to store indx with most possible size
+    scanf(" %d",&c);
     if(c==1){
         char name[50];
-        printf("Enter name: ");
-        scanf("%s", name);
+        printf("\nEnter name: ");
+        scanf(" %s", name);
+        int found=0;
         for(int i=0, a=0; i<*userNum;i++){
-            for(int j=0; j<strlen(name);j++){
-                if(name[j]!=users[i].name[j])continue;
+            
+                if(strstr(users[i].name,name)){
+                    n[a++]=i;
+                    found++;
             }
-            n[a++]=i;
+        }
+        if(found){
+            printf("\n\tSearch Result:\n\n");
+        for(int i=0; i<found; i++){
+            printf("%d. %s\n",i+1,users[n[i]].name);
+        }
+        printf("\nChoose the user number from above to see user's profile. Type 0 to return: ");
+        scanf(" %d",&c);
+        if(c){
+        profile(n, c, users);
+        }
+        
+
+        }
+        else{
+            printf("User not found.\n");
         }
     }
 }
+
+void profile(int n[], int c, User users[]){
+    //need to load user account details here
+    printf("\tUser Profile:\n");
+    printf("Name: %s\n", users[n[c-1]].name);
+    printf("Id: %d\n", users[n[c-1]].id);
+    printf("Dept: %s\n", users[n[c-1]].dept);
+    printf("Semester: %d\n", users[n[c-1]].sem);
+    printf("Books borrowed: %d\n", users[n[c-1]].booksKept);
+    printf("Due: %d\n", users[n[c-1]].due);
+    printf("Status: %s\n", users[n[c-1]].status);
+    printf("\n\n\tChoose an option from below:\n");
+    printf("1. Edit User Profile\n2. Lent Books\n3. Return Books\n4. Return to menu\nEnter your choice: ");
+    scanf(" %d", &c);
+    
+    switch(c) {
+        case 1:
+            // Add edit profile functionality
+            break;
+        case 2:
+            // Add lent books functionality
+            break;
+        case 3:
+            // Add return books functionality
+            break;
+        case 4:
+            return;
+        default:
+            printf("Invalid choice!\n");
+            break;
+    }
+}
+
+
+
+/* Plan
+1. make a book structure
+2. make sure to use some of books elemnnt with user such as books keeping duration.
+3. make a profile loading function
+4. make sure to update dues, book kept duration etc with current date
+5. use file handling for better data arrangement
+*/
